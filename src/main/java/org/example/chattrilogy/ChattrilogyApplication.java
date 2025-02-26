67d481775d049a1680386fd68ad77ced88c7bfac
dan <minhdan190904@gmail.com>
2025-02-26 10:37:32 +0700

    wew

---
+++ src/main/java/org/example/chattrilogy/ChattrilogyApplication.java
@@ -0,0 +1,13 @@
+package org.example.chattrilogy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattrilogyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattrilogyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/chattrilogy/ChattrilogyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chattrilogy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattrilogyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
